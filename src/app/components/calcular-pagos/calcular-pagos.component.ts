8f1764cc977da38aa2bca4d5ccc19590aa3575b7
PabloAsmad15 <pabloasmad15@gmail.com>
2024-07-12 16:53:49 -0500

    feat:calculadora

---
+++ src/app/components/calcular-pagos/calcular-pagos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calcular-pagos',
+  standalone: true,
+  imports: [],
+  templateUrl: './calcular-pagos.component.html',
+  styleUrl: './calcular-pagos.component.css'
+})
+export class CalcularPagosComponent {
+
+}
